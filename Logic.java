ddfb344cd94a35b518160261ff3677d3cb77ada3
tinwy <tinweiyang@gmail.com>
2014-09-25 18:04:38 +0800

    Added a basic structure to Logic
    You can find the detailed breakdown on the google drive document. Will
    attach to this page soon

--- Logic.java
+++ Logic.java
@@ -1,1 +1,46 @@
 //This is how we roll
+import java.util.*;
+import java.io.*;
+
+public class Logic {
+	// EpiphanyMain contains all the current projects which is stored in an array List
+	public static main void(String[] args) {
+	ArrayList<ArrayList<Task>> EpiphanyMain = new ArrayList<ArrayList<Task>>();
+	
+	}
+}
+
+class EpiphanyLogicFunctions {
+	
+	
+
+}
+
+
+class Task {
+	
+	public class Task() {
+		String instruction;
+		String deadLine;
+		String ProjectName;
+		boolean isCompleted:
+	}
+	
+	//Accessors
+	String getInstruction() {
+		return this.instruction;
+	}
+	
+	String getDeadline() {
+		return this.deadLine;
+	}
+	
+	//Mutators	
+	void setInstruction(String newInstruction) {
+		this.instruction = newInstruction;
+	}
+	
+	void setDeadLine(String newDeadline) {
+		this.deadLine = newDeadline;
+	}
+}
