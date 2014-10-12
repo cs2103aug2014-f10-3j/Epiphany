012ead5ccb2712e28a6ba86b116559bf95e45ef7
Abdulla Contractor <mac_abdulla@hotmail.com>
2014-10-13 02:12:26 +0800

    Javadocs for UIHandler class

--- Logic/UIHandler.java
+++ Logic/UIHandler.java
@@ -1,3 +1,9 @@
+/**
+ * This is a singleton class that can be instantiated and used to perform
+ * all display to the user interface.
+ * 
+ * @author abdulla contractor and amit gamane
+ */
 package Logic;
 
 public class UIHandler{
@@ -6,6 +12,10 @@ public class UIHandler{
 	private UIHandler() {
 	}
 	
+	/**
+	 * Obtain a
+	 * @return
+	 */
 	public static UIHandler getInstance(){
 		if(uiHandler == null){
 			uiHandler = new UIHandler();
