72a7056b66994ad872c36da5c37fdd10f7ea5d5e
Amit <dragnoth46@gmail.com>
2014-10-19 22:35:54 +0800

    Updated Task.java
    Refactored

--- Logic/Engine/Task.java
+++ Logic/Engine/Task.java
@@ -4,15 +4,15 @@ package Logic.Engine;
 import java.util.*;
 
 /**
- * This class aims to help in the creation and management of tasks. Each task contains an instruction,
- * a deadline of class Date, a projectName which is the name of the project that the task is stored
- * under and a boolean variable to check if the task has been completed.
+ * This class helps in the creation and management of tasks. 
+ * Each task contains a task description, a deadline of class Date, a projectName and
+ * a boolean variable to check if the task has been completed.
  * 
  * @author Moazzam  and Wei Yang
- *
+ * edited by @author amit
  */
 public class Task {
-		private String instruction;
+		private String taskDescription;
 		private Date deadLine;
 		private String projectName;
 		private boolean isCompleted;
@@ -20,38 +20,44 @@ public class Task {
 		/**
 		 * Overloaded constructors for the creation of tasks are shown below. They differ in the 
 		 * type of arguments that they receive.
-		 * @param instruction	stores the actual task	
+		 * @param taskDescription 
 		 * @param date			stores the deadline
 		 * @param ProjectName	stores the name of the project that the task belongs to
 		 */
 		public Task(String instruction, Date date, String ProjectName) {
-			this.instruction = instruction;
+			this.taskDescription = instruction;
 			this.deadLine = date;
-
 			this.projectName = ProjectName;
 			this.isCompleted = false; // Set as false as a default
 		}
 		
 		public Task(String instruction){
-			this.instruction = instruction;
-			
+			this.taskDescription = instruction;
+			this.deadLine = null;
+			this.projectName = "default";
+			this.isCompleted = false;
 		}
-		public Task(String instruction, Date date){
-			this.instruction = instruction;
-			this.deadLine = date;
-		}
-		public Task(String instruction, String projectName){
-			this.instruction = instruction;
-			this.projectName = projectName;
-			
-		}
-
 		
-		public String getInstruction() {
-			return this.instruction;
+		public Task(String instruction, Date date){
+			this.taskDescription = instruction;
+			this.deadLine = date;
+			this.projectName = "default";
+			this.isCompleted = false;
+		}
+		
+		public Task(String instruction, String projectName){
+			this.taskDescription = instruction;
+			this.deadLine = null;
+			this.projectName = projectName;
+			this.isCompleted = false;
 		}
 
-		Date getDeadline() {
+/**********************Getters******************************/		
+		public String getTaskDescription() {
+			return this.taskDescription;
+		}
+
+		public Date getDeadline() {
 			return this.deadLine;
 		}
 
@@ -63,26 +69,30 @@ public class Task {
 			return isCompleted;
 		}
 
-		// Mutators
-		void setInstruction(String newInstruction) {
-			this.instruction = newInstruction;
+/**********************Mutators******************************/	
+		
+		public void setInstruction(String newInstruction) {
+			this.taskDescription = newInstruction;
 		}
 
-		void setDeadLine(Date newDate) {
+		public void setDeadLine(Date newDate) {
 			this.deadLine = newDate;
 		}
 
-		void setProjectName(String newProjectName) {
+		public void setProjectName(String newProjectName) {
 			this.projectName = newProjectName;
 		}
 
-		void isCompletedUpdate(boolean value) {
-			this.isCompleted = value;
+		public boolean isFinished() {
+			this.isCompleted = true;
+			return this.isCompleted;
 		}
 		
+/**********************Other Methods******************************/	
+
 		@Override
 		public String toString() {
-			return this.getInstruction() + "has the deadline: " + this.getDeadline();
+			return this.getTaskDescription() + "has the deadline: " + this.getDeadline();
 		}
 
 		
